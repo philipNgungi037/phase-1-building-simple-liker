0a6a9ce8a598f911b3bf629181e52e6041efe09d
PHILIP <philophilo037@gmail.com>
2022-09-06 13:03:34 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -2,8 +2,26 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+function initialize(){
+  let likes = document.getElementsByClassName('like-glyph')
+  console.log(likes)
+  for (const element of likes){
+    element.addEventListener('click', (event)=> {
+      mimicServerCall()
+      .then(()=> event.target.textContent = heartChange[event.target.textContent])
+      .catch(error =>{
+        let modal = document.getElementById('modal');
+        modal.classList.remove('hidden')
+        modal.querySelector('h2').textContent = error
+        setTimeout(function(){modal.classList.add('hidden')}, 3000)
 
+      })
+      // (change heart status)event.target.textContent = heartChange[event.target.textContent]
+    }
+    )
+  }
+}
+initialize()
 
 
 
